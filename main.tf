17ba413b5915b9fa56ad2cf83626549f775374f4
Palaniappan, Krishnappan <kpalaniappan@levi.com>
2023-01-03 20:00:45 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ provider "aws" {
     profile = "krishp"
 }
 
-
 resource "aws_ecr_repository" "ecr-repo" {
   name                 = "ecr-repo"
   image_tag_mutability = "MUTABLE"
@@ -22,3 +21,61 @@ resource "aws_ecr_repository" "ecr-repo" {
   }
 }
 
+resource "aws_ecs_cluster" "ecr-repo" {
+  name = "ecr-repo-cluster" # Naming the cluster
+}
+
+resource "aws_ecs_task_definition" "ecr-repo" {
+  family                   = "ecr-repo-task" # Naming our first task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "ecr-repo-task",
+      "image": "${aws_ecr_repository.ecr-repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "my_first_service" {
+  name            = "my-first-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.ecr-repo.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.ecr-repo-task.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
+}
